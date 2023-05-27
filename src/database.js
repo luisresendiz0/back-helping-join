22f0ce1786d64550448217ce0780a73fb03641e6
luis.resendiz <luis.resendiz@digitex.ltd>
2023-05-26 18:36:55 -0600

    sql config

--- src/database.js
+++ src/database.js
@@ -2,11 +2,11 @@ import mysql from "mysql2/promise";
 
 const useConnection = async () => {
   const connection = await mysql.createPool({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "db_helping_join",
+    host: process.env.MYSQLHOST || "localhost",
+    port: process.env.MYSQLPORT || 3306,
+    user: process.env.MYSQLUSER || "root",
+    password: process.env.MYSQLPASSWORD || "root",
+    database: process.env.MYSQLDATABASE || "db_helping_join",
   });
 
   return connection;
