4edd57bfd80a7bf9a1389b77a8dd830203a6dc97
luis.resendiz <luis.resendiz@digitex.ltd>
2023-05-29 01:33:53 -0600

    fix no recomendation

--- src/api/recomendations/controllers/getRecomendations.js
+++ src/api/recomendations/controllers/getRecomendations.js
@@ -1,6 +1,5 @@
 import useConnection from "../../../database";
 
-
 const getRecomendations = async (req, res) => {
   const response = {
     success: false,
@@ -21,7 +20,7 @@ const getRecomendations = async (req, res) => {
 
     let ids = "(";
 
-    recomendations.forEach(r => {
+    recomendations.forEach((r) => {
       ids += r.id_evento + ",";
     });
 
@@ -33,27 +32,28 @@ const getRecomendations = async (req, res) => {
     FROM evento e
     INNER JOIN evento_categoria ec ON e.id_evento = ec.id_evento
     INNER JOIN categoria c ON ec.id_categoria = c.id_categoria
-    WHERE e.id_evento IN ${ids}
-    GROUP BY e.nombre, ec.id_evento;`
+    WHERE e.id_evento IN ${ids.length > 1 ? ids : "(0)"}
+    GROUP BY e.nombre, ec.id_evento;`;
 
     const resultRecomendaciones = await connection.query(queryRecomendaciones);
 
     const unsortedEventos = resultRecomendaciones[0];
 
-    const sortedEventos = recomendations.map(r => unsortedEventos.find(e => e.id_evento === r.id_evento));
+    const sortedEventos = recomendations.map((r) =>
+      unsortedEventos.find((e) => e.id_evento === r.id_evento)
+    );
 
     await connection.end();
-response.success = true;
+    response.success = true;
     response.message = "Recomendaciones generadas";
     response.data = sortedEventos;
     res.status(200).json(response);
-
   } catch (error) {
     console.log(error);
     response.success = false;
     response.message = error.message;
     res.status(500).json(response);
   }
-}
+};
 
 export default getRecomendations;
