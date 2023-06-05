a64fde88755afd87a34c1a10a6b65e155e461f5c
luis.resendiz <resendiz4800@gmail.com>
2023-06-05 00:12:39 -0600

    search v2

--- src/api/search/controllers/search.js
+++ src/api/search/controllers/search.js
@@ -11,86 +11,83 @@ export const search = async (req, res) => {
 
   const {
     text, // texto a buscar
-    type, // eventos, organizaciones, civiles independientes
-    categoryId, // categoria de evento u organizacion
-    fecha_inicio, // fecha de inicio de evento
-    fecha_fin, // fecha de fin de evento
-    entidad, // estado de evento u organizacion
-    alcaldia, // ciudad de evento u organizacion
+    type, // 'eventos', 'organizaciones', 'civiles'
   } = req.body;
-
+  console.log(text)
   try {
-    // if(!text) throw new Error('No se ha enviado texto a buscar');
-    if (!text) throw new Error("No se ha enviado tipo de busqueda");
 
-    let searchQuery = "";
-
-    if (type === "evento") {
-      // buscar eventos
-
-      // SELECT id_evento, id_beneficiado, nombre, descripcion, fecha_inicio, fecha_fin, calle, numero_exterior, numero_interior, colonia, alcaldia, codigo_postal, entidad, imagen FROM db_helping_join.evento;
-      searchQuery = `
-      SELECT e.*, GROUP_CONCAT(c.id_categoria, ':', c.nombre SEPARATOR ', ') AS categorias
+    const eventosQuery = `
+      SELECT e.*, GROUP_CONCAT(c.nombre SEPARATOR ', ') AS categorias
       FROM evento e
       INNER JOIN evento_categoria ec ON e.id_evento = ec.id_evento
       INNER JOIN categoria c ON ec.id_categoria = c.id_categoria
-      WHERE 
-        ${
-          text
-            ? `(e.nombre LIKE '%${text}%'OR e.descripcion LIKE '%${text}%')`
-            : ""
-        }
- 
-      GROUP BY e.id_evento
-      `;
+      ${
+        text
+          ? `WHERE (e.nombre LIKE '%${text}%' OR e.descripcion LIKE '%${text}%')`
+          : ""
+      }
+      GROUP BY e.id_evento;
+    `;
 
-      //   const queryRecomendaciones = `
-      // SELECT e.*, GROUP_CONCAT(c.id_categoria, ':', c.nombre SEPARATOR ', ') AS categorias
-      // FROM evento e
-      // INNER JOIN evento_categoria ec ON e.id_evento = ec.id_evento
-      // INNER JOIN categoria c ON ec.id_categoria = c.id_categoria
-      // WHERE e.id_evento IN ${ids}
-      // GROUP BY e.nombre, ec.id_evento;`
-    }
-
-    // (fecha_inicio >= '${fecha_inicio}'
-    // OR fecha_fin <= '${fecha_fin}'
-    // OR entidad LIKE '%${entidad}%'
-    // OR alcaldia LIKE '%${alcaldia}%'
-    // OR ec.id_categoria = ${categoryId})
-
-    if (type === "organizacion" || type === "civil") {
-      // buscar organizaciones o civiles
-
-      // SELECT id_beneficiado, nombre, imagen, tipo, calle, numero_exterior, numero_interior, colonia, alcaldia, codigo_postal, entidad, telefono, descripcion, responsable, email, contrasena, evento_eliminados FROM db_helping_join.beneficiado;
-      searchQuery = `
-      SELECT b.*
+    const organizacionesQuery = `
+      SELECT b.*, GROUP_CONCAT(c.nombre SEPARATOR ', ') AS categorias
       FROM beneficiado b
       INNER JOIN beneficiado_categoria bc ON b.id_beneficiado = bc.id_beneficiado
-      WHERE 
-        ${
-          text
-            ? `(nombre LIKE '%${text}%' OR descripcion LIKE '%${text}%' OR responsable LIKE '%${text}%')`
-            : ""
-        }
-        
-      GROUP BY b.id_beneficiado
-      `;
-    }
+      INNER JOIN categoria c ON bc.id_categoria = c.id_categoria
+      WHERE
+      ${
+        text
+          ? `(b.nombre LIKE '%${text}%' OR b.descripcion LIKE '%${text}%' OR b.responsable LIKE '%${text}%') AND`
+          : ""
+      }
+        b.tipo = 'organizacion'
+      GROUP BY b.id_beneficiado;
+    `;
 
-    // (tipo = '${type}'
-    //     OR entidad LIKE '%${entidad}%'
-    //     OR alcaldia LIKE '%${alcaldia}%'
-    //     OR bc.id_categoria = ${categoryId})
+    const civilesQuery = `
+      SELECT b.*, GROUP_CONCAT(c.nombre SEPARATOR ', ') AS categorias
+      FROM beneficiado b
+      INNER JOIN beneficiado_categoria bc ON b.id_beneficiado = bc.id_beneficiado
+      INNER JOIN categoria c ON bc.id_categoria = c.id_categoria
+      WHERE
+      ${
+        text
+          ? `(b.nombre LIKE '%${text}%' OR b.descripcion LIKE '%${text}%' OR b.responsable LIKE '%${text}%') AND`
+          : ""
+      }
+        b.tipo = 'civil'
+      GROUP BY b.id_beneficiado;
+    `;
 
     const connection = await useConnection();
 
-    const [searchResult] = await connection.query(searchQuery);
+    let eventos = []
+    let organizaciones = []
+    let civiles = []
+
+    if(type.includes("eventos") || type.includes("all")) {
+      const eventosResult = await connection.query(eventosQuery);
+      eventos = eventosResult[0];
+    }
+
+    if(type.includes("organizaciones") || type.includes("all")) {
+      const organizacionesResult = await connection.query(organizacionesQuery);
+      organizaciones = organizacionesResult[0];
+    }
+
+    if(type.includes("civiles") || type.includes("all")) {
+      const civilesResult = await connection.query(civilesQuery);
+      civiles = civilesResult[0];
+    }
 
     await connection.end();
     response.success = true;
     response.message = "Busqueda exitosa";
-    response.data = searchResult;
+    response.data = {
+      eventos,
+      organizaciones,
+      civiles
+    };
     return res.status(200).json(response);
   } catch (error) {
     console.error(error);
