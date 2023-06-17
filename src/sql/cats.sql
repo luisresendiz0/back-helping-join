88bdedcd3ea33b9cbcea8e0f19e2d468bb96302b
Luis Resendiz <resendiz4800@gmail.com>
2023-06-16 19:54:51 -0600

    cats.sql

---
+++ src/sql/cats.sql
@@ -0,0 +1,20 @@
+INSERT INTO railway.categoria (nombre) VALUES
+	 ('Niños y adolescentes'),
+	 ('Personas mayores'),
+	 ('Vivienda'),
+	 ('Comedores'),
+	 ('Educación'),
+	 ('Adicciones'),
+	 ('Enfermedades'),
+	 ('Personas con discapacidad'),
+	 ('Indigencia'),
+	 ('Reinserción social');
+INSERT INTO railway.categoria (nombre) VALUES
+	 ('Medio ambiente'),
+	 ('Animales'),
+	 ('Administración'),
+	 ('Comunicación'),
+	 ('Derecho'),
+	 ('Diseño'),
+	 ('Psicología y Psicometría'),
+	 ('Tecnología');
